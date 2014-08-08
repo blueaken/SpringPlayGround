69068e182bf7db17b3dda82284d9ce6e8c3055d7
BruceShen <blueaken@gmail.com>
2014-08-07 18:41:55 -0700

    step 2 of the tutorial so far

--- SpringMVC/SpringApp/src/main/webapp/WEB-INF/jsp/index.jsp
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <title>Inventory System</title>
-</head>
-<body>
-<h2>Spring App</h2>
-
-</body>
-</html>
--- SpringMVC/SpringApp/src/test/java/com/blueaken/springmvc/HelloControllerTest.java
+++ SpringMVC/SpringApp/src/test/java/com/blueaken/springmvc/HelloControllerTest.java
@@ -2,6 +2,7 @@ package com.blueaken.springmvc;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -13,7 +14,10 @@ public class HelloControllerTest {
     public void testHandleRequestView() throws Exception{
         HelloController controller = new HelloController();
         ModelAndView modelAndView = controller.handleRequest(null, null);
-        assertEquals("hello.jsp", modelAndView.getViewName());
+        assertEquals("hello", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        String nowValue = (String) modelAndView.getModel().get("now");
+        assertNotNull(nowValue);
     }
 }
 
